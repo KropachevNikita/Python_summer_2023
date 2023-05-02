c73c20b31278e18f608f319f35d80cc132d718c1
KropachevNikita <nik.kropa4ew@yandex.ru>
2023-05-02 22:59:26 +0300

    task_2_2

---
+++ Task_2_1.py
@@ -0,0 +1,7 @@
+n=int(input ("Введите число n - "))
+for i in range(1):
+    print(n,"* 1 =", 1*n)
+    print(n,"* 2 =", 2*n)
+    print(n,"* 3 =", 3*n)
+    print(n,"* 4 =", 4*n)
+    print(n,"* 5 =", 5*n)
---
+++ Task_2_2.py
@@ -0,0 +1,6 @@
+lst=[11,2,15,6,9,17,1,15,21]
+a=lst[0]
+for i in lst:
+    if i<a:
+        a=i
+print(a)
